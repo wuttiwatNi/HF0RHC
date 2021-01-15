a9faed39b01b6176af5bdd46b2f50d5b3eb29d5a
fluke-monkey <wuttiwat.n@gmail.com>
2021-01-16 01:14:12 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { MatTableDataSource } from '@angular/material/table'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'section2question2';
+  displayedColumns: string[] = ['data']
+  dataSource = new MatTableDataSource<String>()
+
+  url = 'https://api.publicapis.org/categories'
+
+  constructor(private http: HttpClient) {
+    this.http.get(this.url).subscribe((res) => {
+      this.dataSource.data = res as String[]
+    })
+  }
+
+  applyFilter(event: Event) {
+    this.dataSource.filterPredicate = (
+      data: String,
+      filter: string
+    ): boolean => {
+      return data.trim().toLowerCase().includes(filter)
+    }
+
+    const filterValue = (event.target as HTMLInputElement).value
+    this.dataSource.filter = filterValue.trim().toLowerCase()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatTableModule } from '@angular/material/table'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
